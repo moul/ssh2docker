b7e6170419286ea4c8a203720148401c095e85ba
Manfred Touron <m@42.am>
2015-09-16 09:23:01 +0200

    Now using codegangstal/cli

--- cmd/ssh2docker/main.go
+++ cmd/ssh2docker/main.go
@@ -2,12 +2,52 @@ package main
 
 import (
 	"net"
+	"os"
+	"path"
 
 	"github.com/Sirupsen/logrus"
+	"github.com/codegangsta/cli"
 	"github.com/moul/ssh2docker"
 )
 
+var VERSION string
+
 func main() {
+	app := cli.NewApp()
+	app.Name = path.Base(os.Args[0])
+	app.Author = "Manfred Touron"
+	app.Email = "https://github.com/moul/ssh2docker"
+	app.Version = VERSION
+	app.Usage = "SSH portal to Docker containers"
+
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "debug, D",
+			Usage: "Enable debug mode",
+		},
+		cli.StringFlag{
+			Name:  "bind, b",
+			Value: ":2222",
+			Usage: "Listen to address",
+		},
+	}
+
+	app.Action = Action
+
+	app.Run(os.Args)
+}
+
+func hookBefore(c *cli.Context) error {
+	// logrus.SetOutput(os.Stderr)
+	if c.Bool("debug") {
+		logrus.SetLevel(logrus.DebugLevel)
+	} else {
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+	return nil
+}
+
+func Action(c *cli.Context) {
 	server, err := ssh2docker.NewServer()
 	if err != nil {
 		logrus.Fatalf("Cannot create server: %v", err)
@@ -18,11 +58,12 @@ func main() {
 		logrus.Fatalf("Cannot add host key file: %v", err)
 	}
 
-	listener, err := net.Listen("tcp", ":2222")
+	bindAddress := c.String("bind")
+	listener, err := net.Listen("tcp", bindAddress)
 	if err != nil {
-		logrus.Fatalf("Failed to start listener: %v", err)
+		logrus.Fatalf("Failed to start listener on %q: %v", bindAddress, err)
 	}
-	logrus.Infof("Listening on port 2222")
+	logrus.Infof("Listening on %q", bindAddress)
 
 	for {
 		conn, err := listener.Accept()
