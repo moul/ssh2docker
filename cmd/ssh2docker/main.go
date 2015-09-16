a11ec17c0271e8b4132cc71832a92acebecb033e
Manfred Touron <m@42.am>
2015-09-16 09:44:49 +0200

    Now using a default SSH host key, may be overwritten using cli flags

--- cmd/ssh2docker/main.go
+++ cmd/ssh2docker/main.go
@@ -12,6 +12,38 @@ import (
 
 var VERSION string
 
+// Default key to ease getting started with the server
+// You can easily use your own key by setting up
+// `--host-key=/path/to/id_rsa`.
+// See `man 1 ssh-keygen`.
+const DefaultHostKey = `-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEA0BGfZSFn5ueRzMGPnd4+QkbrJ5vRmRXdg0D3ukSxFQC+QCXM
+5lVYDtqp6DSsiIIr3PB0n94onebdDK7763RO1/fJP0ZBN3Ih1q0oQ9llrq7kuOd/
+ttjNviu9KAkVQLcfR6zxttIPu/xnwkn7Y0pNOpn6ytjA2whemEKTAyskLSNVBqtW
+r2TY/am7aXYG+1HSkbfSTSKI4ekzHAFLAZGK1q4FDOMAs6kC4IEmop1T3O2LvPBF
+QzTt2WT0kph4+4saMqo4yoKEcKbdnWRkZul1YOcVyJReFX78fCKGo9tVjwtHHa3C
+98WgjUiAXN2boGY2tPqk5vrTVmB69CJ6reezKQIDAQABAoIBAEBtRILnDio0iDPz
+t4m1mGejWAtCt2sElzueMVcPEBolycNJMSIdSRAIa1YIgWgfjn9yQVqDSuZh5w6X
+XFAzCnrbMgiSs3z8rTexFGe1+ENXymDq5ePzS/nXx1GPRnJsgZYLGil28AJQjLxf
+diTvi+xaY4rOBSGNfOT+sFDp2eDTofTbxidgDzEJe0tWMi9QHs5NkyERYO7cpd6I
+uXe0QLMP0aBzMKH4BFMiyBcY2gxYxqr1rC7YmEmn6M2HKxsHjGJ9K7msyJ0CdXNk
+tiqwi3++T3jOcOz3u1t921/wUb4P7TJmerduEt5fm6wJVPCwZdQBcmoMznC0FGb2
+5UwxeaUCgYEA7PTCQi7nxJB56CTxgM6m3D/+Lzdq1SBgUt3kzKPtJtDAfSCBd4hL
+NYNyJ2WqJBdoclY+FQq85Rn2EdZY7Baol3WK26y+xWgXrICzPAtBvNByrr7eFpJS
+2c35PrEuvJvsx0yn2HM4a8JYAm2yq+iJuW0aUNquhFLECvivsCfmXjcCgYEA4MqG
+Rxy2Xu47UELfG3GONe6qd9gv2WWMzXtGXQfzYcYQMFUtfAj04/5ER92BEVqVTARB
+ZOhdgLPTHsP4FwcjsCpjZQ2cW6QXUQfGJGrCWWd+3tFSv7ekrp4mXl4iXT/zJnWB
+BrbABDS8qwz2+YhV6ql2wlYbVI7wdAf4tev2yZ8CgYBQNWmsTYRWnTEmy5qUJ1+E
+HoVEJlYbXqI8arAQNU0JXpBJyr8IXzJWIvB5NYiqPuI0Ec1iAgh+5JLO5ueiwui+
+nCMsyQSqfdnFoqsJICZYa5bmX+V9bnptD7PW7NMNNRqpO+F0+0uV7mssJ0XbuxMj
+mTLXO67nS7zgmd2em2L3cQKBgAFYNMVoHo8izagFPmBjpX4dF1fwKxkZymXQPvN/
+gK0tChu/5q2/P/e9JZtob8UyzYHO5LU9zpFegfzFH07D9CqxljachjrmGF2btkux
+d8ghHlkm11/eMVX6DDC0T3BPWZz5RvRLU4qy5g3/3dpQPnNQ4Cz5ZuBymm2XPp2X
+87nxAoGBAIh+WKnvopNektUbJX7hDE1HBVVaFfM3VffNDfBYF9ziPotRqY+th30N
+Nn/aVy1yLb3M0eZCFD9A9/NE23kXAIFMAjM44NpqtXU/Z6Dmc+qB0Lk+KSL72cQL
+hLP2sDRnSOAAGEYHcN75mvdrKahDRQXD8l0FIRUn/BNTlob8g5Tp
+-----END RSA PRIVATE KEY-----`
+
 func main() {
 	app := cli.NewApp()
 	app.Name = path.Base(os.Args[0])
@@ -22,13 +54,18 @@ func main() {
 
 	app.Flags = []cli.Flag{
 		cli.BoolFlag{
-			Name:  "debug, D",
-			Usage: "Enable debug mode",
+			Name:  "verbose, V",
+			Usage: "Enable verbose mode",
 		},
 		cli.StringFlag{
 			Name:  "bind, b",
 			Value: ":2222",
 			Usage: "Listen to address",
+		},
+		cli.StringFlag{
+			Name:  "host-key, k",
+			Usage: "Path or complete SSH host key to use",
+			Value: "built-in",
 		},
 	}
 
@@ -39,7 +76,7 @@ func main() {
 
 func hookBefore(c *cli.Context) error {
 	// logrus.SetOutput(os.Stderr)
-	if c.Bool("debug") {
+	if c.Bool("verbose") {
 		logrus.SetLevel(logrus.DebugLevel)
 	} else {
 		logrus.SetLevel(logrus.InfoLevel)
@@ -47,17 +84,25 @@ func hookBefore(c *cli.Context) error {
 	return nil
 }
 
+// Action is the default cli action to execute
 func Action(c *cli.Context) {
+	// Initialize the SSH server
 	server, err := ssh2docker.NewServer()
 	if err != nil {
 		logrus.Fatalf("Cannot create server: %v", err)
 	}
 
-	err = server.AddHostKeyFile("/Users/moul/Git/moul/ssh2docker/host_rsa")
+	// Register the SSH host key
+	hostKey := c.String("host-key")
+	if hostKey == "built-in" {
+		hostKey = DefaultHostKey
+	}
+	err = server.AddHostKey(hostKey)
 	if err != nil {
-		logrus.Fatalf("Cannot add host key file: %v", err)
+		logrus.Fatalf("Cannot add host key: %v", err)
 	}
 
+	// Bind TCP socket
 	bindAddress := c.String("bind")
 	listener, err := net.Listen("tcp", bindAddress)
 	if err != nil {
@@ -65,6 +110,7 @@ func Action(c *cli.Context) {
 	}
 	logrus.Infof("Listening on %q", bindAddress)
 
+	// Accept new clients
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
--- ssh2docker.go
+++ ssh2docker.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"os"
 	"os/exec"
+	"strings"
 	"sync"
 	"syscall"
 
@@ -27,17 +28,28 @@ func NewServer() (*Server, error) {
 	return &server, nil
 }
 
-func (s *Server) AddHostKeyFile(keypath string) error {
-	keystring, err := ioutil.ReadFile("host_rsa")
+// AddHostKey parses/loads an ssh key and registers it to the server
+func (s *Server) AddHostKey(keystring string) error {
+	// Check if keystring is a key path or a key string
+	keypath := os.ExpandEnv(strings.Replace(keystring, "~", "$HOME", 2))
+	_, err := os.Stat(keypath)
+	var keybytes []byte
+	if err == nil {
+		keybytes, err = ioutil.ReadFile(keypath)
+		if err != nil {
+			return err
+		}
+	} else {
+		keybytes = []byte(keystring)
+	}
+
+	// Parse SSH priate key
+	hostkey, err := ssh.ParsePrivateKey(keybytes)
 	if err != nil {
 		return err
 	}
 
-	hostkey, err := ssh.ParsePrivateKey(keystring)
-	if err != nil {
-		return err
-	}
-
+	// Register key to the server
 	s.sshConfig.AddHostKey(hostkey)
 	return nil
 }
