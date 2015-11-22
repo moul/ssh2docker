42c82c6d5653b87d78797dcf2d850b7e2cb558c7
Manfred Touron <m@42.am>
2015-11-22 16:45:26 +0100

    Bump homebrew

--- contrib/homebrew/ssh2docker.rb
+++ contrib/homebrew/ssh2docker.rb
@@ -3,8 +3,8 @@ require "language/go"
 class Ssh2docker < Formula
   desc "SSH server that creates a Docker container per connection (chroot++)"
   homepage "https://github.com/moul/ssh2docker"
-  url "https://github.com/moul/ssh2docker/archive/v1.1.0.tar.gz"
-  sha256 "4838f01676221f4b0df490049cc27df5dde1ae083628148ec900bc0cef950e6a"
+  url "https://github.com/moul/ssh2docker/archive/v1.2.0.tar.gz"
+  sha256 "712f9ba6200bcf741785bc0ce2c2b77de21c4c15d18f3e5475b8ff5e08a10df6"
 
   head "https://github.com/moul/ssh2docker.git"
 
