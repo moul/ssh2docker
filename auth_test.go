8e9fc253a50a03311cb275213c83c0174a5c1d35
Quentin Perez <qperez@ocs.online.net>
2016-06-03 14:32:44 +0200

    travis: fix auth_test.go

--- auth_test.go
+++ auth_test.go
@@ -1,12 +1,17 @@
 package ssh2docker
 
 import (
+	"os"
 	"testing"
 
+	"github.com/apex/log"
+	"github.com/apex/log/handlers/text"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestServer_CheckConfig(t *testing.T) {
+	log.SetHandler(text.New(os.Stderr))
+
 	Convey("Testing Server.CheckConfig", t, FailureContinues, func() {
 		// FIXME: check with a script
 		server, err := NewServer()
