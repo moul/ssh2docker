ffb99fa93b63e48465dd79e877adcb02a938f117
Quentin Perez <qperez@ocs.online.net>
2016-06-03 14:18:54 +0200

    godep: update dependencies

---
+++ vendor/github.com/apex/log/handlers/text/text.go
@@ -0,0 +1,101 @@
+// Package text implements a development-friendly textual handler.
+package text
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"sync"
+	"time"
+
+	"github.com/apex/log"
+)
+
+// Default handler outputting to stderr.
+var Default = New(os.Stderr)
+
+// start time.
+var start = time.Now()
+
+// colors.
+const (
+	none   = 0
+	red    = 31
+	green  = 32
+	yellow = 33
+	blue   = 34
+	gray   = 37
+)
+
+// Colors mapping.
+var Colors = [...]int{
+	log.DebugLevel: gray,
+	log.InfoLevel:  blue,
+	log.WarnLevel:  yellow,
+	log.ErrorLevel: red,
+	log.FatalLevel: red,
+}
+
+// Strings mapping.
+var Strings = [...]string{
+	log.DebugLevel: "DEBUG",
+	log.InfoLevel:  "INFO",
+	log.WarnLevel:  "WARN",
+	log.ErrorLevel: "ERROR",
+	log.FatalLevel: "FATAL",
+}
+
+// field used for sorting.
+type field struct {
+	Name  string
+	Value interface{}
+}
+
+// by sorts projects by call count.
+type byName []field
+
+func (a byName) Len() int           { return len(a) }
+func (a byName) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a byName) Less(i, j int) bool { return a[i].Name < a[j].Name }
+
+// Handler implementation.
+type Handler struct {
+	mu     sync.Mutex
+	Writer io.Writer
+}
+
+// New handler.
+func New(w io.Writer) *Handler {
+	return &Handler{
+		Writer: w,
+	}
+}
+
+// HandleLog implements log.Handler.
+func (h *Handler) HandleLog(e *log.Entry) error {
+	color := Colors[e.Level]
+	level := Strings[e.Level]
+
+	var fields []field
+
+	for k, v := range e.Fields {
+		fields = append(fields, field{k, v})
+	}
+
+	sort.Sort(byName(fields))
+
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	ts := time.Since(start) / time.Second
+	fmt.Fprintf(h.Writer, "\033[%dm%6s\033[0m[%04d] %-25s", color, level, ts, e.Message)
+
+	for _, f := range fields {
+		fmt.Fprintf(h.Writer, " \033[%dm%s\033[0m=%v", color, f.Name, f.Value)
+	}
+
+	fmt.Fprintln(h.Writer)
+
+	return nil
+}
